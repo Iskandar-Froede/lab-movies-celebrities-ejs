a8a11a3828e8553c3854fc34c0e499c6b07244d8
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-10-31 16:15:44 +0100

    it2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,19 @@
 //  Add your code here
+const { Schema, model } = require('mongoose')
+
+const CelebritySchema = new Schema ({
+  name: {
+    type: String,
+  },
+  occupation: {
+    type: String,
+  },
+  catchPhrase: {
+    type: String,
+  },
+
+})
+
+const Celebrity = model('celebrity', CelebritySchema)
+
+module.exports = Celebrity
